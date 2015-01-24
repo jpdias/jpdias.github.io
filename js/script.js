9ff8e44eb39b9c388baf407c3583e62d46e69a76
Joao Pedro Dias <jpdias@live.com.pt>
2015-01-24 00:49:28 +0000

    intro sample projs and animations

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  if (!$(".menu ").is(":visible "))
+    $('#show').html('<i class="fa fa-angle-double-down "></i> Menu');
+  else
+    $('#show').html('<i class="fa fa-angle-double-up "></i> Menu');
+
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    //  if ($(window).width() < 720) {
+    menu();
+    //  }
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
+    });
+
+  });
+
+  $('#show').click(function(e) {
+    menu();
+    e.preventDefault();
+  });
+
+});
+
+function menu() {
+  $('.menu').slideToggle("fast ", function() {
+    if ($(this).is(':visible')) {
+      $('#show').html('<i class="fa fa-angle-double-up "></i> Menu');
+    } else {
+      $('#show').html('<i class="fa fa-angle-double-down "></i> Menu');
+    }
+  });
+}
